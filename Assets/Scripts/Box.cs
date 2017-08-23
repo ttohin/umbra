95d57720c013861e3f1bf5b6add8316aa743bb7a
Anton Simakov <ttohin@gmail.com>
2017-08-23 21:45:46 +0200

    Added player bullets, breakable boxes and houses

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour {
+
+	private int health = 2;
+	public Sprite brokenSprite;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (health == 0)
+			return;
+
+		if (other.gameObject.tag == "Bullet") {
+
+			Destroy (other.gameObject);
+
+			health -= 1;
+			if (health == 0) {
+				GetComponent<SpriteRenderer> ().sprite = brokenSprite;
+				Destroy (GetComponent<BoxCollider2D> ());
+			}
+		}
+	}
+}
--- Assets/Scripts/MilitaryRid.cs
+++ Assets/Scripts/MilitaryRid.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class MilitaryRid : MonoBehaviour {
 
 	private MilitarySceneController sceneController;
+    private Animator animator;
 	public float speed = 0.1f;
+	public GameObject bulletPrefab;
 
 	// Use this for initialization
 	void Start () {
 		sceneController = GameObject.Find ("/GameController").GetComponent<MilitarySceneController> ();
+        animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +21,30 @@ public class MilitaryRid : MonoBehaviour {
 		var verticalAxis = Input.GetAxis ("Vertical");
 		var horizontalAxis = Input.GetAxis ("Horizontal");
 
-		transform.position += new Vector3 (horizontalAxis * speed, verticalAxis * speed);
+		var offset = new Vector3 (horizontalAxis * speed, verticalAxis * speed);
+
+		animator.SetBool("isWalking", offset != Vector3.zero);
+		GetComponent<SpriteRenderer> ().flipX = offset.x > 0;
+
+		transform.position += offset;
+
+		if (Input.GetMouseButtonDown (0)) {
+			Fire ();
+		}
+	}
+
+	private void Fire()
+	{
+		var bullet = (GameObject)Instantiate(
+			bulletPrefab,
+			transform.position,
+			Quaternion.identity);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 5.0f);
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f); 
 	}
 		
 	void OnTriggerEnter2D(Collider2D other) {
